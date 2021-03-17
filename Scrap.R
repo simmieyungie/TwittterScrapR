89345199d83d5319ca0538d6f338c551a21c1a26
Simmieyungie <56706551+simmieyungie@users.noreply.github.com>
2021-03-17 17:11:57 +0100

    Update

---
+++ Scrap.R
@@ -0,0 +1,28 @@
+
+#load library
+library(twitteR)
+library(utils)
+
+#Save app credentials for access to tweets
+consumerKey <- "TLwcYDEblVFgtDIqeqGXY05zH"
+
+consumerSecret <- "Wpy8VEH6eGliqisMCNonqC7q9Z3YrxpmjxkiIyMdSj8qsocdSw"
+
+accessToken <- "1097552545411678208-zIYNJm6l1aOwxUwCtLbgbpmjGciJtp"
+
+accessTokenSecret <-  "d3V7AQ2BSkcj5qCZIjuSeuOPpsfUfM3eRb1YyMmlLdoPG"
+
+#set up 
+setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
+
+
+#
+#data collector
+data <- searchTwitter("uber", n = 1000, 
+                      resultType = "recent", lang = "en")
+
+
+
+#write csv
+write.csv(twListToDF(data), paste("data/","uber", Sys.Date(), ".csv", sep = ""))
+
